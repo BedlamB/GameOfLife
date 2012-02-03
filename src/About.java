d980bd5819849eb55e6dff261dcefa4792b5b384
unknown <vicks@libhelp-pc.(none)>
2012-02-03 13:43:03 +0000

    'project init'

---
+++ src/About.java
@@ -0,0 +1,15 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vicks
+ * Date: 03/02/12
+ * Time: 12:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class About extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
